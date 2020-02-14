141ebdcd4627dac65e5aa6fd691fa5c7a03f1d17
Sebastien La Duca <sladuca777@gmail.com>
2020-02-14 17:18:23 -0500

    hydrate on client

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import routes from './router'
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -5,8 +7,18 @@ addEventListener('fetch', event => {
  * Respond with hello worker text
  * @param {Request} request
  */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
+async function handleRequest (event) {
+  const u = new URL(event.request.url)
+  const { header, footer, component } = routes[u.pathname]
+  const body = ReactDOMServer.renderToString(React.cloneElement(component, { name: 'World' }))
+  return new Response(header + body + footer, {
+    headers: {
+      'Content-Type': 'text/html'
+    }
   })
+}
+
+if (typeof navigator !== "undefined") {
+	const app = document.querySelector("#app");
+	ReactDOM.hydrate(React.cloneElement(routes[location.pathname], {name:"World"} ), app);
 }
